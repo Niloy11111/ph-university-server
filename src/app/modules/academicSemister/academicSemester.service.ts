3e65c2e143b04677fd97f82696a59c4bc1bed374
ekhthiar_uddin <ektiaruddinniloy859@gmail.com>
2024-12-04 23:02:58 +0600

    $1

--- src/app/modules/academicSemister/academicSemester.service.ts
+++ src/app/modules/academicSemister/academicSemester.service.ts
@@ -1,8 +1,15 @@
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemister.interface';
 import { AcademicSemester } from './academicSemister.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
   // vaiya TAcademicSemesterCode import kore pelse video 12.7 a
+
+  // academicSemesterNameCodeMapper['Fall'] = 3
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
